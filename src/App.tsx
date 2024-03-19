682f93242e8809fe064a3e844ff79efb11156580
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2024-03-19 21:35:48 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 
-import { ButtonMenu, Input } from './lib'
-
 function App(): React.JSX.Element {
     return (
-        <div>
-            <ButtonMenu icon="bookmark">Click me</ButtonMenu>
-            <Input label={'djdkdjk'}/>
-        </div>
+        <div />
     )
 }
 
