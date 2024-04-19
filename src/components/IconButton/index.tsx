9cbafadf7e419c1fd5d5157a2ee9321e74e49595
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2024-04-19 18:16:22 +0200

    fix IconButton

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -57,7 +57,7 @@ export class IconButton extends React.Component<Partial<IconButtonProps & Toolti
             ...otherProps,
             theme,
             className: className,
-            icon: iconIsString
+            icon: !iconIsString
                 ? icon
                 : null,
             'data-label': label,
