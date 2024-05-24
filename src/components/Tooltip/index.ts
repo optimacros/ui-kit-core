389c81ba8f4bfa1c87f6ae35d4ee624bef81744a
feex-s <feex.mail@gmail.com>
2024-05-24 17:42:38 +0300

    reexport Tooltip types

---
+++ src/components/Tooltip/index.ts
@@ -0,0 +1,5 @@
+export type { Position } from './Tooltip'
+export type { PositionInfo } from './Tooltip'
+export type { TooltipTheme } from './Tooltip'
+export type { TooltipProps } from './Tooltip'
+export { Tooltip } from './Tooltip'
