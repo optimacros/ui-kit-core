32a05a747eec11d6c6cd2189e9eb7e719dfa8500
Elena Kara <e.kara@stellarlabs.ai>
2024-05-28 15:30:51 +0300

    refactor (Icon): stories + readme

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,24 +1,24 @@
-import { Meta, StoryObj } from '@storybook/react'
+import { Meta, StoryObj } from "@storybook/react"
 
-import { Icon } from './index'
+import { Icon } from "./index"
 import { ArgumentType } from "../../types/ArgumentType.ts";
 
 const argTypes: ArgumentType = {
     value: {
-        control: 'text',
-        description: 'Value of the icon. Using `FontIcon` if value string. Or `div` if value node.'
+        control: "text",
+        description: "Value of the icon. Using `FontIcon` if value string. Or `div` if value node."
     },
     title: {
-        control: 'text',
-        description: 'Icon description, visible on icon hover.'
+        control: "text",
+        description: "Icon description, visible on icon hover."
     },
     alt: {
-        control: 'text',
-        description: 'The text used to set the `aria-label` attribute.'
+        control: "text",
+        description: "The text used to set the `aria-label` attribute."
     },
     style: {
-        control: 'object',
-        description: 'Add styles to component.'
+        control: "object",
+        description: "Add styles to component."
     },
     className: {
         table: { disable: true }
@@ -31,7 +31,7 @@ const argTypes: ArgumentType = {
 
 
 const meta: Meta<typeof Icon> = {
-    title: 'UI Kit lite/Icon',
+    title: "UI Kit lite/Icon",
     component: Icon,
     argTypes
 }
@@ -39,26 +39,37 @@ export default meta
 
 type Story = StoryObj<typeof Icon>
 
+const UserIcon = () => (
+    <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" >
+        <path d="m12 11c2.2091 0 4-1.79086 4-4s-1.7909-4-4-4c-2.20914 0-4 1.79086-4 4s1.79086 4 4 4zm0 10c3.866 0 7-1.7909 7-4s-3.134-4-7-4c-3.86599 0-7 1.7909-7 4s3.13401 4 7 4z" fill="#28303f" clipRule="evenodd" fillRule="evenodd" />
+    </svg>
+)
 
 export const Basic: Story = {
     args: {
-        value: 'close',
-        alt: 'close icon'
+        value: "close",
+        alt: "close icon"
     },
 }
 
 export const WithTooltip: Story = {
     args: {
-        value: 'search',
-        title: 'description',
+        value: "search",
+        title: "description",
     },
 }
 
 export const CustomStyles: Story = {
     args: {
-        value: 'add',
-        title: 'description',
-        style: { border: '1px solid black' }
+        value: "add",
+        title: "description",
+        style: { border: "1px solid black" }
+    },
+}
+
+export const CustomIcon: Story = {
+    args: {
+        value: <UserIcon />,
     },
 }
 
--- src/components/Icon/readme.md
+++ src/components/Icon/readme.md
@@ -5,17 +5,8 @@
 import { Icon } from 'ui-kit-lite/components/Icon';
 
 const UserIcon = () => (
-    <svg fill="none"
-         height="24"
-         viewBox="0 0 24 24"
-         width="24"
-         xmlns="http://www.w3.org/2000/svg"
-    >
-        <path d="m12 11c2.2091 0 4-1.79086 4-4s-1.7909-4-4-4c-2.20914 0-4 1.79086-4 4s1.79086 4 4 4zm0 10c3.866 0 7-1.7909 7-4s-3.134-4-7-4c-3.86599 0-7 1.7909-7 4s3.13401 4 7 4z"
-              fill="#28303f"
-              clipRule="evenodd"
-              fillRule="evenodd"
-        />
+    <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" >
+        <path d="m12 11c2.2091 0 4-1.79086 4-4s-1.7909-4-4-4c-2.20914 0-4 1.79086-4 4s1.79086 4 4 4zm0 10c3.866 0 7-1.7909 7-4s-3.134-4-7-4c-3.86599 0-7 1.7909-7 4s3.13401 4 7 4z" fill="#28303f" clipRule="evenodd" fillRule="evenodd" />
     </svg>
 )
 
