6ea3684faddfdf4214d800b661bbd0ea55f623e2
Elena Kara <e.kara@stellarlabs.ai>
2024-06-24 16:13:31 +0300

    refactor (config): return copyPublicDir: false

--- vite.config.ts
+++ vite.config.ts
@@ -64,6 +64,7 @@ export default defineConfig({
         },
     },
     build: {
+        copyPublicDir: false,
         lib: {
             entry: path.resolve(__dirname, 'src/components/index.ts'),
             formats: ['es'],
